42e86e2268ff54abc21cc6fc57250b4e022e3877
Karli <nasabisan@gmail.com>
2021-07-08 01:23:51 -0500

    Completo a excepcion del hacker edition

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import validator from './validator.js';
+import validator from './validator.js';4
 
-console.log(validator);
+const input = document.querySelector('#input')
+
+input.addEventListener('keydown', onlyNumbers)
+
+function onlyNumbers(creditCardNumber) {
+let only = /^\d$/
+ if(creditCardNumber.keyCode == only || creditCardNumber.keyCode <= 57 && creditCardNumber.keyCode != 32) {
+      return true;
+}
+creditCardNumber.preventDefault();
+}
+const capture = document.getElementById('capture')
+
+capture.addEventListener('click', function(){
+    let numbersCard = document.getElementById("input").value;
+    let validador = validator.isValid(numbersCard);
+    document.getElementById("input").value = validator.maskify(numbersCard);
+    if(validador === true){
+        document.getElementById("result").innerHTML = "Tarjeta valida";
+    }else{
+        document.getElementById("result").innerHTML = "Tarjeta inválida";
+    }
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
 const validator = {
-  // ...
+  isValid: function (creditCardNumber) {
+    if(!creditCardNumber) return false
+    let NumbersCard = []
+    NumbersCard = creditCardNumber.split("").reverse().map(function(number, index){
+        if(index%2===1){
+            let valueDouble = number * 2
+            if (valueDouble >= 10) {
+            return valueDouble-9;
+              }
+            return number * 2
+        }
+        return parseInt(number)
+        
+    })
+    let total = 0;
+    for (let i = 0; i < NumbersCard.length; i++){
+      total = total += NumbersCard[i]; 
+    }
+    if (total%10===0){
+      return true;
+    } else {
+      return false;
+    }
+  },
+  maskify: function (creditCardNumber){
+    let ocultado = []
+    for (let i = 0; i < creditCardNumber.length; i++){
+      if(i < creditCardNumber.length-4){
+        ocultado.push("#");
+        } else {
+        ocultado.push(creditCardNumber[i]);
+      }
+    }
+    return ocultado.join("");
+  } 
 };
 
 export default validator;
